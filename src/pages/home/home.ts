0f44fc05b2e92c8b59003262142d4f7887cf685c
Justin <justin_ellery@hotmail.ca>
2017-01-21 21:57:33 -0500

    Clear button working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,12 +10,32 @@ export class HomePage {
   private _value: number;
   private _lastButtonPressed: string;
 
-
+  // Default Constructor
   constructor() {
     // set default value to 0
-    this._value = 0.1;
+    this._value = 0;
+  }
+
+
+  // Operands
+  addOperand($event){
 
   }
 
+  // Operators 
+  
+
+  // CLEAR Event Handler
+  clear($event, role){
+    console.log($event);    
+  }
+
+  // EQUALS Event Handler
+  equals($event)
+  {
+    
+  }
+
+  
 
 }
